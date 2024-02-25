d67f6206d5b11ad2aa57c04a5b5be8f3488e4afa
Carlos07NG <corzuelachaco2019@gmail.com>
2024-02-25 16:46:33 -0300

    product udpate

--- src/controllers/productsController/update.js
+++ src/controllers/productsController/update.js
@@ -1,32 +1,86 @@
-const { leerJSON, escribirJSON } = require('../../data')
-const { existsSync, unlinkSync } = require('fs')
-
+const { existsSync, unlinkSync } = require("fs");
+const db=require('../../database/models');
 
 module.exports = (req, res) => {
-    const { name, price, category, description, offer, discount } = req.body;
+    const mainImage = req.files.mainImage;
+    const images = req.files.images;
 
-    const { id } = req.params
+    const {
+    name,
+    price ,
+    categoryId, 
+    description, 
+    offer,
+    discount
+    } = req.body
 
-    const {mainImage, images} = req.files;
+    const {id} = req.params
 
-    const products = leerJSON('products');
-
-    const productsUpdated = products.map(product => {
-        if (product.id == id) {
-            product.name = name.trim();
-            product.price = price;
-            product.mainImage = mainImage ? mainImage[0].filename : null;
-            product.images = images ? images.map((image) => image.filename) : [];
-            product.category = category;
-            product.description = description.trim();
-            product.offer = offer;
-            product.discount = discount;
+    db.Product.findByPk(id,{
+        include : ['images']
+    }).then(()=>{
+        if(images){
+            existsSync("public/images/" + product.images) &&
+            unlinkSync("public/images/" + product.images) 
         }
-        return product
+
+        db.Product.update(
+            {
+                name: name.trim(),
+                price,
+                description : description.trim(),
+                offer,
+                discount,
+                mainImage : mainImage ? mainImage[0].filename : null,
+                categoryId
+            },
+            {
+                where :{
+                    id,
+                }
+            },
+        ).then((product)=>{
+            if (images) {
+                product.images.forEach((image) => {
+                  existsSync("public/images/" + image.file) &&
+                    unlinkSync("public/images/" + image.file);
+                });
+
+                db.Image.destroy({
+                    where : {
+                        id_product : id
+                    }
+                  }).then(() => {
+                    const imagesDB = images.map(image => {
+                        return {
+                            file: image.filename,
+                            id_product : product.id
+                        }
+                    }) 
+    
+                    db.Image.bulkCreate(imagesDB, {
+                        validate : true
+                    }).then(result => {
+                        console.log(result);
+                        return res.redirect("/admin");
+                    })
+                  })
+            }else {
+                // return res.send(id)
+                return res.redirect("/admin");
+
+            }       
+        });
+    }).catch(error => console.log(error));
+    db.Category.findAll({
+        order: [['name']]
     })
-
-
-    escribirJSON(productsUpdated, 'products')
-
-    return res.redirect('/admin')
+        .then(categories => {
+          return res.render("products/product-add", {
+            errors: errors.mapped(),
+            old: req.body,
+            categories,
+        });
+    })
+    .catch(error => console.log(error))
 }
