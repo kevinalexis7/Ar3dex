943054a0091ed7150125123e658cb23973f13d5b
Carlos07NG <corzuelachaco2019@gmail.com>
2024-02-25 22:16:51 -0300

    product update

--- src/controllers/productsController/update.js
+++ src/controllers/productsController/update.js
@@ -1,86 +1,81 @@
 const { existsSync, unlinkSync } = require("fs");
-const db=require('../../database/models');
+const db = require('../../database/models');
 
-module.exports = (req, res) => {
-    const mainImage = req.files.mainImage;
-    const images = req.files.images;
+module.exports = async (req, res) => {
+    try {
+        const mainImage = req.files.mainImage;
+        const images = req.files.images;
 
-    const {
-    name,
-    price ,
-    categoryId, 
-    description, 
-    offer,
-    discount
-    } = req.body
+        const {
+            name,
+            price,
+            categoryId,
+            description,
+            offer,
+            discount
+        } = req.body;
 
-    const {id} = req.params
+        const { id } = req.params;
 
-    db.Product.findByPk(id,{
-        include : ['images']
-    }).then(()=>{
-        if(images){
-            existsSync("public/images/" + product.images) &&
-            unlinkSync("public/images/" + product.images) 
+        const product = await db.Product.findByPk(id, {
+            include: ['images']
+        });
+
+        if (!product) {
+            return res.status(404).send("Producto no encontrado");
         }
 
-        db.Product.update(
+        if (images) {
+            existsSync("public/images/" + product.images) &&
+            unlinkSync("public/images/" + product.images);
+        }
+
+        await db.Product.update(
             {
                 name: name.trim(),
                 price,
-                description : description.trim(),
-                offer,
+                description: description.trim(),
+                offer: +offer,
                 discount,
-                mainImage : mainImage ? mainImage[0].filename : null,
+                mainImage: mainImage ? mainImage[0].filename : null,
                 categoryId
             },
             {
-                where :{
+                where: {
                     id,
                 }
             },
-        ).then((product)=>{
-            if (images) {
-                product.images.forEach((image) => {
-                  existsSync("public/images/" + image.file) &&
-                    unlinkSync("public/images/" + image.file);
-                });
+        );
 
-                db.Image.destroy({
-                    where : {
-                        id_product : id
-                    }
-                  }).then(() => {
-                    const imagesDB = images.map(image => {
-                        return {
-                            file: image.filename,
-                            id_product : product.id
-                        }
-                    }) 
-    
-                    db.Image.bulkCreate(imagesDB, {
-                        validate : true
-                    }).then(result => {
-                        console.log(result);
-                        return res.redirect("/admin");
-                    })
-                  })
-            }else {
-                // return res.send(id)
-                return res.redirect("/admin");
+        if (images) {
+            for (const image of product.images) {
+                existsSync("public/images/" + image.file) &&
+                unlinkSync("public/images/" + image.file);
+            }
 
-            }       
-        });
-    }).catch(error => console.log(error));
-    db.Category.findAll({
-        order: [['name']]
-    })
-        .then(categories => {
-          return res.render("products/product-add", {
-            errors: errors.mapped(),
-            old: req.body,
-            categories,
-        });
-    })
-    .catch(error => console.log(error))
-}
+            await db.Image.destroy({
+                where: {
+                    id_product: id
+                }
+            });
+
+            const imagesDB = images.map(image => {
+                return {
+                    file: image.filename,
+                    id_product: product.id
+                };
+            });
+
+            await db.Image.bulkCreate(imagesDB, {
+                validate: true
+            });
+
+            return res.redirect("/admin");
+        } else {
+            return res.redirect("/admin");
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Internal Server Error");
+    }
+};
