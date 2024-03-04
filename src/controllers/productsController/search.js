c4a0aaa597ee0e89fba01f99aff3af7329405414
Martín <mcflytix333@gmail.com>
2024-03-04 13:27:18 -0300

    Haciendo que el search pueda buscar por nombre de producto o nombre de categoria

--- src/controllers/productsController/search.js
+++ src/controllers/productsController/search.js
@@ -8,22 +8,27 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 module.exports = (req,res) => {
 
-    const {keyword} = req.query;
+    const { keyword } = req.query;
     db.Product.findAll({
-        where : { [Op.or]:
-            [
-                {name : {[Op.substring] : `%${keyword}%` }},
-                {categoryId: keyword }
+        where: {
+            [Op.or]: [
+                { name: { [Op.substring]: `%${keyword}%` } },
+                {
+                    '$category.name$': { [Op.substring]: `%${keyword}%` }
+                }
             ]
-        }
-        
+        },
+        include: ['category']
     })
-    .then(products => {
-        return res.render('results', {
-            products,
-            keyword,
-            toThousand
-    })
-    })
-    .catch(error => {console.error(error)})
-}
+        .then(products => {
+            return res.render('results', {
+                products,
+                keyword,
+                toThousand
+            });
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send('Error interno del servidor');
+        });
+ }    
