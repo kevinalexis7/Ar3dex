dd97fe6898bf2c28de4e18932f05ddade5ceab0b
Kevinalexis7 <kevinalex.espinola09@gmail.com>
2024-03-21 23:03:57 -0300

    Arreglo en el label de de los fomularios de producto

--- src/middlewares/checkAdmin.js
+++ src/middlewares/checkAdmin.js
@@ -1,7 +1,8 @@
 module.exports = (req,res, next) => {
     if(req.session.userLogin && req.session.userLogin.role === 2){
         return next()
+        return res.redirect('/')
     }
-
-    return res.redirect('/')
+    
+    return next()
 }
