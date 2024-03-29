618eba9cd8b3753ba8c38c9382e3648e18964ffc
Carlos07NG <corzuelachaco2019@gmail.com>
2024-03-28 21:25:48 -0300

    arreglos de error en create y edit

--- src/controllers/productsController/add.js
+++ src/controllers/productsController/add.js
@@ -1,11 +1,16 @@
+const {existsSync, readFileSync} = require('fs');
 const db = require('../../database/models')
+const path = require('path')
 
 module.exports = (req, res) => {
+        const bannerJSON = readFileSync('src/data/banner.json')
+        const {file} = JSON.parse(bannerJSON)
         db.Category.findAll({
                 order: ['name']
         })
         .then(categories => {
                 return res.render("products/product-add",{
+                bannerImage :existsSync('public/images/' + file) ? file : null,
                 categories
                 });
         })
