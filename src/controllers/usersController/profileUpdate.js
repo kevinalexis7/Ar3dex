219c6f93aabdd9a003a40939db52dbc53b23533d
Martín <mcflytix333@gmail.com>
2024-04-08 07:24:12 -0300

    Peleando contra profileUpdate.js

--- src/controllers/usersController/profileUpdate.js
+++ src/controllers/usersController/profileUpdate.js
@@ -1,9 +1,10 @@
-const db = require('../../database/models');
 const { validationResult } = require("express-validator");
+const { existsSync, unlinkSync } = require("fs");
+const db = require('../../database/models');
 
-
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     const userImage = req.files.image;
+
     const {
         name,
         surname,
@@ -18,40 +19,64 @@ module.exports = (req, res) => {
     const errors = validationResult(req);
 
     if (errors.isEmpty()) {
-        db.User.findByPk(req.params.id, {
-            include: ['address']
-        })
-            .then(user => {
-                // Actualizar la dirección
-                return user.address.update({
+        try {
+            const user = await db.User.findByPk(req.params.id, {
+                include: ['address']
+            });
+
+            if (userImage) {
+
+                if (user.image) {
+                    existsSync("public/images/users/" + user.image) &&
+                    unlinkSync("public/images/users/" + user.image);
+                }
+
+                await user.update({
+                    name,
+                    surname,
+                    email,
+                    phone,
+                    image: userImage ? userImage[0].filename : null
+                },
+                {
+                    where: {
+                        id: req.params.id
+                    }
+                }
+            );
+
+                await user.address.update({
                     street,
-                    city : city.toString(),
-                    province : province.toString(),
+                    city: city.toString(),
+                    province: province.toString(),
                     postal_code,
                 });
-            })
-            .then(() => {
-                // Actualizar la información del usuario
-                return db.User.update(
-                    {
-                        name,
-                        surname,
-                        email,
-                        phone,
-                        image: userImage 
-                    },
-                    {
-                        where: {
-                            id: req.params.id
-                        }
-                    }
-                );
-            })
-            .then(() => {
+
                 return res.redirect('/');
-            })
-            .catch(error => console.log(error))
+            } else {
+
+                await user.address.update({
+                    street,
+                    city: city.toString(),
+                    province: province.toString(),
+                    postal_code,
+                });
+
+                await user.update({
+                    name,
+                    surname,
+                    email,
+                    phone
+                });
+
+                return res.redirect('/');
+            }
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send("Error del servidor");
+        }
     } else {
+
         db.User.findByPk(req.params.id, {
             include: ['address']
         })
